501ac64227723bdd2a82bf78eaeaaf371ca97015
NightmareFox12 <carloshenriquez392004@gmail.com>
2025-07-25 17:10:57 -0400

    feat: add useTransactor hook for transaction handling with notifications
    feat: implement useWatchBalance hook to update balance on block changes
    
    feat: create useIsMobile hook for mobile detection
    
    feat: add utility functions for class name merging
    
    feat: add logo SVG asset
    
    feat: include 3D model files for Arbitrum and Optimis
    
    feat: add thumbnail image for the application
    
    feat: implement Zustand store for global state management
    
    feat: configure wagmi for web3 interactions with multiple chains
    
    feat: set up wallet connectors for various wallet integrations
    
    style: add global styles with Tailwind CSS and daisyUI themes
    
    feat: create utility functions for blockchain interactions and metadata handling
    
    feat: implement error parsing utility for better error messages
    
    feat: create custom notification component for user feedback

---
+++ packages/nextjs/services/web3/wagmiConnectors.tsx
@@ -0,0 +1,47 @@
+import { connectorsForWallets } from "@rainbow-me/rainbowkit";
+import {
+  coinbaseWallet,
+  injectedWallet,
+  ledgerWallet,
+  metaMaskWallet,
+  oneInchWallet,
+  rabbyWallet,
+  trustWallet,
+  walletConnectWallet,
+} from "@rainbow-me/rainbowkit/wallets";
+import { rainbowkitBurnerWallet } from "burner-connector";
+import * as chains from "viem/chains";
+import scaffoldConfig from "~~/scaffold.config";
+
+const { onlyLocalBurnerWallet, targetNetworks } = scaffoldConfig;
+
+const wallets = [
+  oneInchWallet,
+  metaMaskWallet,
+  walletConnectWallet,
+  ledgerWallet,
+  coinbaseWallet,
+  injectedWallet,
+  rabbyWallet,
+  trustWallet,
+  ...(!targetNetworks.some(network => network.id !== (chains.hardhat as chains.Chain).id) || !onlyLocalBurnerWallet
+    ? [rainbowkitBurnerWallet]
+    : []),
+];
+
+/**
+ * wagmi connectors for the wagmi context
+ */
+export const wagmiConnectors = connectorsForWallets(
+  [
+    {
+      groupName: "Supported Wallets",
+      wallets,
+    },
+  ],
+
+  {
+    appName: "pay-fusion",
+    projectId: scaffoldConfig.walletConnectProjectId,
+  },
+);
